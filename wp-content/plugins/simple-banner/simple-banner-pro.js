89ab5612601c288833cd706252f21e50b919abc8
Kristjan Mikk <kristjanmikk@hotmail.com>
2019-12-05 14:18:59 +0200

    deleted plugin `Simple Banner` version 2.1.3

--- wp-content/plugins/simple-banner/simple-banner-pro.js
+++
@@ -1,7 +0,0 @@
-// v2.1.3
-if (!window.location.hostname.includes("localhost")) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://simple-banner.glitch.me/hostname", true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({ hostname: window.location.hostname }));
-}
